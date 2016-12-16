70784adc8e26022dd398e7aec8b4f90505da465e
Peter Barabas <z0d@notresp.com>
2016-12-16 08:29:07 +0100

    Ditch displaying date

--- plugins/a38.py
+++ plugins/a38.py
@@ -1,6 +1,5 @@
 import plugin
 import fetcher
-import time
 from lxml import html
 
 
@@ -9,11 +8,10 @@ def a38(irc, user, target, msg):
     data = fetcher.get_page(url, 'a38.html')
     tree = html.fromstring(data)
     path = '//div[@class="etterem-content-napiMenu"]/p[2]/strong/text()'
-    today = time.strftime("%Y.%m.%d")
 
     menu = tree.xpath(path)
     if menu == []:
-        line = 'No menu for %s @ A38' % today
+        line = 'No menu found @ A38'
         irc.msg(target, line)
         return
 
@@ -21,7 +19,7 @@ def a38(irc, user, target, msg):
     menu = ''.join(menu)
     menu = menu.replace('\n', ' | ')
 
-    line = 'Menu for %s @ A38: %s' % (today, menu)
+    line = 'Current A38 menu: %s' % menu
     irc.msg(target, line.encode('ascii', 'replace'))
 
 plugin.add_plugin('^!a38\Z', a38)
