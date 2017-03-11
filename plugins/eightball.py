f988dd1776be0f3cf5f36a02a3ec9ac5bbfc240e
Alexandra Vass <alexandra.vass@ericsson.com>
2017-03-11 20:08:04 +0100

    Add the megic 8ball plugin

---
+++ plugins/eightball.py
@@ -0,0 +1,40 @@
+import plugin
+import random
+
+
+def eightball(irc, user, target, msg):
+    items = msg.split(' ')
+
+    if len(items) <= 1: #no questions to answer...
+        irc.msg(target, user + ': Please, ask your question!')
+        return
+     
+    answers = [
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes definitely',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most likely',
+    'Outlook good',
+    'Yes',
+    'Signs point to yes',
+    'Reply hazy try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don\'t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+    ]
+
+    rnd = random.randint(0, len(answers) - 1)
+    irc.msg(target, user+': ' + answers[rnd])
+
+plugin.add_plugin('^!eightball ', decide)
+plugin.add_help('!eightball',
+                'The Magic 8-Ball is a toy used for fortune-telling or seeking advice, developed in the 1950s. Example: !eightball Will I have a great day?')
