a51cc5f22d4136d188d4412c1d9b6baae2d33bfb
Alexandra Vass <alexandra.vass@ericsson.com>
2017-03-22 19:29:49 +0100

    [WIP] make the colormapping, convert colorcodes

--- plugins/wttr.py
+++ plugins/wttr.py
@@ -1,5 +1,6 @@
 import plugin
 import requests
+import re
 
 
 def wttr(irc, user, target, msg):
@@ -17,16 +18,40 @@ def wttr(irc, user, target, msg):
     
     
     if content[0].startswith("ERROR"):
-        item = nick +": Can't find the city. :("
+        item = nick +": content[0] :("
     else:
         item = '\n'.join(content[2:7])
+        item = change_ansi_to_irc(item)
     
     irc.msg(target, item)
 
 plugin.add_plugin('^!wttr ', wttr)
 plugin.add_help('!wttr',
-                'Fancy weather forecast using wttr.it')
+                'Fancy weather forecast using wttr.it Example: !wttr Budapest')
 
-def colorconvert():
-    #256 to irc
-    pass
+def change_ansi_to_irc(strcontent):
+   return strcontent
+
+def change_ansi_graph_to_irc(ansi_graphic_string): 
+    ansicolor = ansi_graphic_string.split(";")[2].replace("m","")
+    irccolor = convert_colors(ansicolor)
+    return "\x03" + irccolor
+
+def convert_colors(ansicolor):
+    for k,v in ansi_colors_in_irc().items():
+        if ansicolor in k:
+            return v
+    return "00"
+
+def ansi_colors_in_irc():
+    return {
+        ("15") : "00",
+        ("21") : "02",
+        ("202") : "04",
+        ("154","190", "226", "220", "228")  : "08",
+        ("46", "82", "118") : "09",
+        ("45","48","47") :"10",
+        ("27", "33", "39", "111") : "12",
+        ("214", "208") : "13",
+        ("250", "251", "240", "255") : "15"
+        }
