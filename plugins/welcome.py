3ff2fcd7f7c280429c7a0d5162017d1c99251fe8
Peter Barabas <z0d@notresp.com>
2018-06-02 11:43:18 +0200

    Don't spam welcome messages

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,12 +1,18 @@
 import plugin
 import re
+import time
 import common
 
 
 WELCOME_FILE = 'welcome.yaml'
+WELCOME_TIME = 4 * 3600
+
+# store timestamps for welcomes
+memory = {}
 
 
 def welcome(irc, user, channel, msg=None):
+
     try:
         welcomes = common.parse_config('plugins/' + WELCOME_FILE)
     except Exception:
@@ -15,9 +21,11 @@ def welcome(irc, user, channel, msg=None):
 
     for regexp, message in welcomes.iteritems():
         regexp = re.compile(regexp, re.I)
-        if re.search(regexp, user):
+        timestamp = memory.get(regexp, 0)
+        if re.search(regexp, user) and timestamp < time.time() - WELCOME_TIME:
             line = '[%s] ' % user
             line += message
             irc.msg(channel, line)
+            memory[regexp] = time.time()
 
 plugin.add_plugin('', welcome, 'join')
