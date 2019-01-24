5d00d5104cc6a8b241a709e475ceae4d41b3978a
Peter Barabas <z0d@notresp.com>
2019-01-24 23:07:08 +0100

    Add plugin to display Python version

---
+++ plugins/version.py
@@ -0,0 +1,13 @@
+import plugin
+import time
+from sys import version_info as vi
+
+
+def j_version(irc, user, target, msg):
+    v1 = vi.major
+    v2 = vi.minor
+    v3 = vi.micro
+    irc.msg(target, "I'm running on Python {}.{}.{}".format(v1, v2, v3))
+
+plugin.add_plugin('^!ver\Z', j_version)
+plugin.add_help('!ver', 'Show Python version')
