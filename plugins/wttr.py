1714f46fd2d9855abb52924c2ee62e5d86ab20cd
skully <alexa.vass@gmail.com>
2020-07-22 22:29:26 +0200

    mehmehmehmeh

--- plugins/wttr.py
+++ plugins/wttr.py
@@ -44,7 +44,7 @@ class Wttr:
     @staticmethod
     def change_ansi_to_irc(strcontent):
         strcontent_resetcolor_changed = re.sub('\\033\[\dm', '\x03', strcontent)
-        all_ansi_colors = set(re.findall(r'(\033[^m]*m)', strcontent_resetcolor_changed)) #reduce the list, change to set?
+        all_ansi_colors = set(re.findall(r'(\033[^m]*m)', strcontent_resetcolor_changed))
         for color in all_ansi_colors:
             irc_color = Wttr.change_ansi_graph_to_irc(color)
             strcontent = strcontent.replace(color, irc_color)
@@ -76,41 +76,5 @@ class Wttr:
             ("27", "33", "39", "111"): "12",
             ("214", "208"): "13",
             ("250", "251", "240", "255"): "15"
-=======
-def change_ansi_to_irc(strcontent):
-    # reset color
-    strcontent = re.sub('\\033\[\dm','\x03' , strcontent)
-    ansi_colors = re.findall(r'(\033[^m]*m)', strcontent) 
-    for color in ansi_colors:
-        strcontent = strcontent.replace(color,change_ansi_graph_to_irc(color))
-   
-    print strcontent
-    return strcontent
 
-def change_ansi_graph_to_irc(ansi_graphic_string): 
-    ansicolor = ansi_graphic_string.split(";")
-    ansicolor = ansicolor[2].replace("m","")
-    irccolor = convert_colors(ansicolor)
-
-    print ansi_graphic_string, ansicolor, irccolor
-    return "\x03" + irccolor
-
-def convert_colors(ansicolor):
-    for k,v in ansi_colors_in_irc().items():
-        if ansicolor in k:
-            return v
-    return "00"
-
-def ansi_colors_in_irc():
-    return {
-        ("15") : "00",
-        ("21") : "02",
-        ("202") : "04",
-        ("154","190", "226", "220", "228")  : "08",
-        ("46", "82", "118") : "09",
-        ("45","48","47") :"10",
-        ("27", "33", "39", "111") : "12",
-        ("214", "208") : "13",
-        ("250", "251", "240", "255") : "15"
->>>>>>> 9a313bbbfb98285cd80677171bcc9bbdd91f14e3
         }
